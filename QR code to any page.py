a319fb0fdab70a7c751a27d24363a6b877a201d4
Matus18 <106945625+Matus18@users.noreply.github.com>
2022-07-17 19:11:55 -0400

    Update and rename QR code to Instagram Page.py to QR code to any page.py

--- QR code to any page.py
+++ QR code to any page.py
@@ -1,6 +1,6 @@
 import pyqrcode
 import png
 
-link = 'https://www.instagram.com/yourusername/' #Put your username
+link = '#Here put the link to create a qr' 
 qr_code = pyqrcode.create(link)
 qr_code.png("Instagram.png", scale =5) #.png create the qr code image
