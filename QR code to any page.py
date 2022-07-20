7dbc843f942e9c76d1cc7db36c634881c3940863
Matus18 <106945625+Matus18@users.noreply.github.com>
2022-07-19 23:58:51 -0400

    Update QR code to any page.py

--- QR code to any page.py
+++ QR code to any page.py
@@ -3,4 +3,4 @@ import png
 
 link = '#Here put the link to create a qr' 
 qr_code = pyqrcode.create(link)
-qr_code.png("Instagram.png", scale =5) #.png create the qr code image
+qr_code.png(".png", scale =5) #.png create the qr code image
