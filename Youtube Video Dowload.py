a5b17e0a0975dda698c1dfbe3f4dcd01729c58a1
Matus18 <106945625+Matus18@users.noreply.github.com>
2022-07-27 23:32:38 -0400

    Update Youtube Video Dowload.py

--- Youtube Video Dowload.py
+++ Youtube Video Dowload.py
@@ -2,5 +2,8 @@ from pytube import YouTube
 
 link = str(input("Introduce en Link: "))
 yt = YouTube(link)
+#GET VIDEO
 ys = yt.streams.get_highest_resolution()
+#GET AUDIO
+ys = yt.streams.get_audio_only()
 ys.download()
